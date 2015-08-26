7bc85a9225d0c03432f6435b39d12a7b87306586
Morten Wulff <wulff@ratatosk.net>
2015-08-26 15:30:06 +0200

    Scan all controller files

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ rm -Rf ./tmpl_cache/*
 php gen_template_cache.php
 popd
 
-xgettext --default-domain=core -o ../datawrapper-localization/locale/messages.pot --from-code=UTF-8 -n --omit-header -k__ -L PHP ./controller/*.php ./scripts/tmpl_cache/*.php ./www/index.php ./www/api/index.php ./lib/api/*.php ./lib/api/*/*.php ./lib/core/build/classes/datawrapper/*.php ./lib/session/*.php ./lib/templates/*.php ./lib/utils/*.php
+xgettext --default-domain=core -o ../datawrapper-localization/locale/messages.pot --from-code=UTF-8 -n --omit-header -k__ -L PHP ./controller/*.php ./controller/account/*.php ./controller/chart/*.php ./scripts/tmpl_cache/*.php ./www/index.php ./www/api/index.php ./lib/api/*.php ./lib/api/*/*.php ./lib/core/build/classes/datawrapper/*.php ./lib/session/*.php ./lib/templates/*.php ./lib/utils/*.php
 
 for dir in ./plugins/*; do
     plugin=${dir:10}
